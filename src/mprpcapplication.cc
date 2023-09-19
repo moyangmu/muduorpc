a3a6336bf471b172741a0e32fa2291f9bc13be0d
moyang <729227315@qq.com>
2023-09-19 19:56:26 +0800

    first commit

---
+++ src/mprpcapplication.cc
@@ -0,0 +1,66 @@
+#include "mprpcapplication.h"
+#include <iostream>
+#include <unistd.h>
+#include <string>
+
+MprpcConfig MprpcApplication::m_config;
+
+void ShowArgsHelp()
+{
+    std::cout<<"format: command -i <configfile>" << std::endl;
+}
+
+void MprpcApplication::Init(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        ShowArgsHelp();
+        exit(EXIT_FAILURE);
+    }
+
+    int c = 0;
+    std::string config_file;
+
+//     getopt 函数的返回值有以下几种可能的取值：
+
+// 如果解析到了一个有效的选项字符，返回该选项字符。
+// 如果解析到了一个无效的选项字符（即不在选项字符串中），返回 ?（问号字符）。
+// 如果解析到了一个需要参数的选项字符，但未提供参数值，返回 :（冒号字符）。
+// 如果解析完所有的选项，返回 -1，表示没有更多的选项需要解析。
+    while((c = getopt(argc, argv, "i:")) != -1)
+    {
+        switch (c)
+        {
+        case 'i':
+            config_file = optarg;
+            break;
+        case '?':
+            ShowArgsHelp();
+            exit(EXIT_FAILURE);
+        case ':':
+            ShowArgsHelp();
+            exit(EXIT_FAILURE);
+        default:
+            break;
+        }
+    }
+
+    // 开始加载配置文件了 rpcserver_ip=  rpcserver_port   zookeeper_ip=  zookepper_port=
+    m_config.LoadConfigFile(config_file.c_str());
+
+    // std::cout << "rpcserverip:" << m_config.Load("rpcserverip") << std::endl;
+    // std::cout << "rpcserverport:" << m_config.Load("rpcserverport") << std::endl;
+    // std::cout << "zookeeperip:" << m_config.Load("zookeeperip") << std::endl;
+    // std::cout << "zookeeperport:" << m_config.Load("zookeeperport") << std::endl;
+}
+
+MprpcApplication& MprpcApplication::GetInstance()
+{
+    static MprpcApplication app;
+    return app;
+}
+
+MprpcConfig& MprpcApplication::GetConfig()
+{
+    return m_config;
+}
